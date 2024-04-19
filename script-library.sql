df598dda1697ed13413fcb23fa8b816dcb4fbd3d
dj <112994984+dexterroberts@users.noreply.github.com>
2024-04-19 16:39:27 -0700

    added 2nd query script

--- script-library.sql
+++ script-library.sql
@@ -3,6 +3,11 @@
 -- Last Modified: 4/19/24
 -- Description: A library of spatial SQL scripts tailored for GIS data analysis.
 
--- Select areas where the statefp (Wisconsin) is equal to '55'
+-- Select counties where the statefp (Wisconsin) is equal to '55'
 SELECT * FROM cb_2019_us_county_500k
+WHERE statefp = '55'
+
+-- Turning polygons into centroids
+SELECT id, st_centroid(geom) AS geom
+FROM cb_2018_us_county_500k
 WHERE statefp = '55'
